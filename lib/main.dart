29931d19c7baa51269470a7b2af87b3e339d20d5
user <lj.matan@gmail.com>
2020-12-11 02:53:10 +0100

    latest

--- lib/main.dart
+++ lib/main.dart
@@ -31,7 +31,10 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Loop Video Player',
-      home: LoopVideoPlayer(),
+      home: Scaffold(
+        backgroundColor: Colors.black,
+        body: LoopVideoPlayer(),
+      ),
     );
   }
 }
@@ -46,8 +49,8 @@ class LoopVideoPlayer extends StatefulWidget {
 class _LoopVideoPlayerState extends State<LoopVideoPlayer> {
   // Video files
   static const String _video1 = 'assets/videos/running.mp4';
-  static const String _video2 = 'assets/videos/running.mp4';
-  static const String _video3 = 'assets/videos/running.mp4';
+  static const String _video2 = 'assets/videos/running2.mp4';
+  static const String _video3 = 'assets/videos/running3.mp4';
 
   // Image files
   static const String _image1 = 'assets/pictures/1.png';
@@ -70,9 +73,7 @@ class _LoopVideoPlayerState extends State<LoopVideoPlayer> {
   void initState() {
     super.initState();
 
-    // Create and store the VideoPlayerController. The VideoPlayerController
-    // offers several different constructors to play videos from assets, files,
-    // or the internet.
+    // Create and store the VideoPlayerControllers.
     _controller2.initialize();
     _controller3.initialize();
 
@@ -136,45 +137,42 @@ class _LoopVideoPlayerState extends State<LoopVideoPlayer> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.black,
-      body: FutureBuilder(
-        future: _initializeVideoPlayerFuture,
-        builder: (context, snapshot) {
-          // If future has error display the error text
-          if (snapshot.hasError)
-            return Center(
-              child: Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 12),
-                child: Text(
-                  snapshot.error.toString(),
-                  textAlign: TextAlign.center,
-                  style: const TextStyle(color: Colors.white),
-                ),
+    return FutureBuilder(
+      future: _initializeVideoPlayerFuture,
+      builder: (context, snapshot) {
+        // If future has error display the error text
+        if (snapshot.hasError)
+          return Center(
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 12),
+              child: Text(
+                snapshot.error.toString(),
+                textAlign: TextAlign.center,
+                style: const TextStyle(color: Colors.white),
               ),
-            );
-          else if (snapshot.connectionState == ConnectionState.done) {
-            // If the VideoPlayerController has finished initialization, use
-            // the data it provides to limit the aspect ratio of the video.
-            _loopVideos();
-            return Center(
-              child: _imageDisplayed != null
-                  ? Image.asset(_imageDisplayed)
-                  : Builder(
-                      builder: (context) => AspectRatio(
-                        aspectRatio: controller.value.aspectRatio,
-                        // Use the VideoPlayer widget to display the video.
-                        child: VideoPlayer(controller),
-                      ),
+            ),
+          );
+        else if (snapshot.connectionState == ConnectionState.done) {
+          // If the VideoPlayerController has finished initialization, use
+          // the data it provides to limit the aspect ratio of the video.
+          _loopVideos();
+          return Center(
+            child: _imageDisplayed != null
+                ? Image.asset(_imageDisplayed)
+                : Builder(
+                    builder: (context) => AspectRatio(
+                      aspectRatio: controller.value.aspectRatio,
+                      // Use the VideoPlayer widget to display the video.
+                      child: VideoPlayer(controller),
                     ),
-            );
-          } else {
-            // If the VideoPlayerController is still initializing, show a
-            // loading spinner.
-            return Center(child: CircularProgressIndicator());
-          }
-        },
-      ),
+                  ),
+          );
+        } else {
+          // If the VideoPlayerController is still initializing, show a
+          // loading spinner.
+          return Center(child: CircularProgressIndicator());
+        }
+      },
     );
   }
 
